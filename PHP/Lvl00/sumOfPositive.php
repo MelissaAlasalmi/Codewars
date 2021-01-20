358c3ed094f3ea581b418ebe647d7b55c9c09e05
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2021-01-20 14:19:41 +0200

    added task description to file

--- PHP/Lvl00/sumOfPositive.php
+++ PHP/Lvl00/sumOfPositive.php
@@ -1,4 +1,6 @@
 <?PHP
+// Take an array of numbers and return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
 	$arr = array(1, 2, 3, 4, 5);
 	$sum = 0;
 	foreach ($arr as &$value) {
--- Python/Lvl00/sumOfPositive.py
+++ Python/Lvl00/sumOfPositive.py
@@ -1,3 +1,5 @@
+# Take an array of numbers and return the sum of all of the positives ones.
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
 def positive_sum(arr):
     sum = 0
     for i in range(0, len(arr)) :
